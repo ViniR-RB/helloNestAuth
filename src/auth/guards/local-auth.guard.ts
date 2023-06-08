ba9c8723c4f4314e944cd96a6a2ec9517cfc2073
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2023-06-07 21:45:36 -0300

    feat(auth): implement login,guard and local

---
+++ src/auth/guards/local-auth.guard.ts
@@ -0,0 +1,20 @@
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class LocalAuthGuard extends AuthGuard('local') {
+  canActivate(context) {
+    return super.canActivate(context);
+  }
+
+  handleRequest(err, user) {
+    if (err || !user) {
+      throw new UnauthorizedException(err?.message);
+    }
+    return user;
+  }
+}
