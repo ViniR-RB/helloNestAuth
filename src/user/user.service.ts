9911db086dd29ec8da40028e2bf7f088567ed671
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2023-06-07 21:10:27 -0300

    feat(user): implement create user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import * as brcypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  constructor(private readonly prisma: PrismaService) {}
+  async create(createUserDto: CreateUserDto) {
+    const data = {
+      ...createUserDto,
+      password: await brcypt.hash(createUserDto.password, 10),
+    };
 
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+    const createdUser = await this.prisma.user.create({
+      data,
+    });
+    return {
+      ...createdUser,
+      password: undefined,
+    };
   }
 }
